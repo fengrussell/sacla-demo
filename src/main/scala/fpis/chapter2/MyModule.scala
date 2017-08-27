417bd0466ca9bf95d270ceeb6f4da7fc89fcc454
russell <russell_feng@163.com>
2017-08-27 23:58:25 +0800

    add MyModules.scala

--- src/main/scala/fpis/chapter2/MyModule.scala
+++ src/main/scala/fpis/chapter2/MyModule.scala
@@ -4,5 +4,71 @@ package fpis.chapter2
   * Created by russell on 2017/8/25.
   */
 class MyModule {
+  def abs(n: Int): Int = {
+    if (n < 0) -n
+    else n
+  }
 
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acct: Int): Int = {
+      if (n <= 0) acct
+      else go(n-1, n*acct)
+    }
+
+    go(n, 1)
+  }
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n+1)
+    }
+
+    loop(0)
+  }
+
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n+1)
+    }
+
+    loop(0)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean = {
+      if ((n+1) >= as.length) true
+      else if (ordered(as(n), as(n+1))) loop(n+1)
+      else false
+    }
+
+    loop(0)
+  }
+
+  def partiall[A, B, C](a: A, f: (A, B) => C): B => C = {
+    (b: B) => f(a, b)
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  }
+
+  def uncurry[A, B, C](f: A => (B => C)): (A, B) => C = {
+    (a: A, b: B) => f(a)
+  }
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
 }
