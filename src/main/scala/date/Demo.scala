baeb86472dd1b30764d5ae55103a86c8719b6f56
fengtaolue <fengtaolue@xiaomi.com>
2017-11-03 20:53:16 +0800

    add code

---
+++ src/main/scala/date/Demo.scala
@@ -0,0 +1,38 @@
+package date
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+object Demo {
+    def main(args: Array[String]): Unit = {
+//        val date=new SimpleDateFormat("yyyy-MM-dd H:mm:ss")
+//        val dateFormated = date.format(line)
+//        val dateFf=date.parse(dateFormated).getTime
+
+//        val date = new Date()
+//        val formater = new SimpleDateFormat("yyyy-MM-dd H:mm:ss")
+//        val formater = new SimpleDateFormat("yyyyMMdd")
+////        println(formater.format(date))
+////        LocalDate.parse("20171022")
+//        var dateFormater = DateTimeFormatter.ofPattern("yyyyMMdd");
+//        val date = LocalDate.parse("20171022", dateFormater)
+//
+//        println(date.minusDays(1).format(dateFormater))
+
+//        val date = "20171010"
+//        new TimeFormat("yyyyMMdd").parse(date).inMilliseconds
+//        val currentDay  = DateTime.parse(date)
+
+        val dateFormater = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val currentDay  = LocalDate.parse("20171021", dateFormater)
+        val yesterday = currentDay.minusDays(1).format(dateFormater)
+        val preWeek = currentDay.minusDays(7).format(dateFormater)
+        val preMonth = currentDay.minusDays(30).format(dateFormater)
+
+        println(yesterday)
+        println(preWeek)
+        println(preMonth)
+    }
+}
--- src/main/scala/demo/EnumTest.scala
+++ src/main/scala/demo/EnumTest.scala
@@ -6,14 +6,16 @@ class EnumTest {
 
 
 object EnumTest extends Enumeration {
-    type EnumTest = Value
+    type Enum = Value
     val One, Two, Three = Value
     val Four = Value(10, "four")
+//    val Five = Enum
 
     def main(args: Array[String]): Unit = {
         println(EnumTest.withName("four").id)
         println(One.id)
         println(Two.id)
+//        println(Five)
 
         println(EnumTest(1))
     }
