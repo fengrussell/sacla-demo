4d4aad31abf8de7e88c83e9d9fa98845e8ce5a55
fengtaolue <fengtaolue@xiaomi.com>
2017-11-06 10:20:00 +0800

    add some SingleClass Demo

--- src/main/scala/de/SingleClass.scala
+++ src/main/scala/de/SingleClass.scala
@@ -1,17 +1,51 @@
 package de
 
-class SingleClass(var para_i: Int = 11) {
-    var var_i: Int = _
-    val val_i: Int = para_i
+//class SingleClass(var para_i: Int = 11) {
+//    var var_i: Int = _
+//    val val_i: Int = para_i
+//    para_i = 1
+//}
+
+class SingleClass1(para_i: Int) {
+    var var_i: Int = 1
+    val val_i: Int = 2
+}
+
+class SingleClass2(var para_i: Int) { // var para_i，等同于在类里定义这样一个var变量。调用SingleClass2(10)，从反编译的结果，不是在构造函数里赋值，应该是直接para_i的方法
+    var var_i: Int = 1
+    val val_i: Int = 2
+}
+
+class SingleClass3(val para_i: Int) {
+    var var_i: Int = 1
+    val val_i: Int = 2
+}
+
+class SingleClass4(para_i: Int) {
+    var var_i: Int = 1
+    val val_i: Int = 2
+
+    // 定义一个新的构造函数
+    def this(i1: Int, i2: Int) {
+        this(i1) // 必须调用默认this构造函数，如果没有定义，就是this()
+        // ......
+    }
+}
+
+class SingleClass5(para_i: Int) {
+    // 这些内容都在构造函数调用
+    println("SingleClass5")
+    test
+    // ------------------
+
+    def test: Unit = {
+        println("SingleClass5.test")
+    }
 }
 
 object SingleClass {
     def main(args: Array[String]): Unit = {
-        val sc = new SingleClass(10)
-
-        sc.var_i = 100
-        println(s"var_i = ${sc.var_i}")
-        println(s"val_i = ${sc.val_i}")
-        println(s"para_i = ${sc.para_i}")
+        val sc = new SingleClass2(10)
+        println(sc.para_i)
     }
 }
