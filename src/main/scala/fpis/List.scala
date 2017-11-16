cf34db2a2e5c32556d0a5d0395813ba49863f496
russell <russell_feng@163.com>
2017-11-17 00:31:41 +0800

    add List demo

---
+++ src/main/scala/fpis/List.scala
@@ -0,0 +1,15 @@
+package fpis
+
+
+sealed trait List[A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(head, tail) => head + sum(tail)
+  }
+}
