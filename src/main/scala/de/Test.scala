0c36168ed6911d84e39d8aceecc61b62c32a42ad
russell <russell_feng@163.com>
2017-11-21 00:31:52 +0800

    add

--- src/main/scala/de/Test.scala
+++ src/main/scala/de/Test.scala
@@ -1,11 +1,20 @@
 package de
 
 class Test {
+    def method1: Unit = {} // 没有参数，编译后和method2一样。但是调用时不能加()，编译不过。
 
+    def method2(): Unit = {}
+
+    var add1 = (x: Int, y: Int) => x + y
+
+    def add2 = (x: Int, y: Int) => x + y
 }
 
 object Test {
   def main(args: Array[String]): Unit = {
-
+      val test = new Test
+      test.method1
+      test.method2
+      test.method2()
   }
 }
